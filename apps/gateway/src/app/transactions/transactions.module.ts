b35785458c1e51d3da0c86ac2e7762e19fff1c18
Abdallah Zidan <azidan@kib.com.kw>
2025-07-05 15:32:11 +0300

    feat: add health check endpoint and update transactions module
    - Introduced `HealthController` with a `/health` endpoint returning a status check.
    - Updated `TransactionsModule` to include the new `HealthController`.
    - Modified the controller index to export the `HealthController`.
    - Updated end-to-end tests to validate the new health check endpoint.

--- apps/gateway/src/app/transactions/transactions.module.ts
+++ apps/gateway/src/app/transactions/transactions.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { TransactionsController } from './controllers';
+import { HealthController, TransactionsController } from './controllers';
 import { TransactionsProcessingService, TransactionsQueryService } from './services';
 import { ProcessorAdapterManagerModule } from '@pemo-task/process-adapter-manager';
 import { ProcessorOneAdapterModule } from '../processor-one-adapter/processor-one-adapter.module';
 
 @Module({
   imports: [ProcessorAdapterManagerModule, ProcessorOneAdapterModule],
-  controllers: [TransactionsController],
+  controllers: [TransactionsController, HealthController],
   providers: [TransactionsProcessingService, TransactionsQueryService],
 })
 export class TransactionsModule {}
